d1a6b3f9e1d49f99dd77c92691f02350359b2cfe
Danil Ovchinnikov <danil42worker@gmail.com>
2022-06-04 20:15:59 +0300

    TEST

--- scripts/license.py
+++ scripts/license.py
@@ -5,9 +5,23 @@ from pathlib import Path
 SOURCE_CODE_FILE_EXTENSIONS = [".h", ".cpp", ".inl"]
 IGNORE_FOLDERS = ["build", "objs", "tests", "scripts"]
 
-LICENCE_RE = re.compile(r"^\/\/ Compiler for PHP \(aka KPHP\)\n"
-                        r"\/\/ Copyright \(c\) \d{4} LLC В«V KontakteВ»\n"
-                        r"\/\/ Distributed under the GPL v3 License, see LICENSE\.notice\.txt")
+LICENCE_RE = re.compile(r"^// Compiler for PHP \(aka KPHP\)\n"
+                        r"// Copyright \(c\) \d{4} LLC «V Kontakte»\n"
+                        r"// Distributed under the GPL v3 License, see LICENSE\.notice\.txt")
+
+
+def _remove_sub_path(full_path: Path, sub_path: Path) -> Path:
+    full_path_str = full_path.as_posix()
+    sub_path_str = sub_path.as_posix()
+
+    if not full_path_str.startswith(sub_path_str):
+        return Path(full_path_str)
+
+    new_path = full_path_str[len(sub_path_str):]
+    if not new_path.startswith("/"):
+        return Path(new_path)
+
+    return Path(new_path[1:])
 
 
 def _get_sources(root_dir: Path):
@@ -16,7 +30,7 @@ def _get_sources(root_dir: Path):
         if file.suffix not in SOURCE_CODE_FILE_EXTENSIONS:
             continue
 
-        path_file_in_project = Path(str(file).removeprefix(str(root_dir) + "\\"))
+        path_file_in_project = _remove_sub_path(file, root_dir)
         if path_file_in_project.parts[0] in IGNORE_FOLDERS:
             continue
 
